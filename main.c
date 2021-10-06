42028d4984813a4a4f2a9797a827b1875a72bfd4
Gabriel Henrique da Silva <65050164+oGabrielSilva@users.noreply.github.com>
2021-10-06 19:01:46 +0000

    Quest one, two and three

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) {
+void quest_one() {
   int number;
   float numberFloat;
   char string[30];
@@ -11,5 +11,38 @@ int main(void) {
   printf("Digite um nome: ");
   scanf("%s", string);
   printf("Você digitou o número %d e o número %f. Depois digitou o nome %s.", number, numberFloat, string);
+}
+
+void quest_two() {
+  float first_n, second_n, m, result;
+  printf("Digite a média necessária para aprovação: ");
+  scanf("%f", &m);
+  printf("Agora digite a primeira nota: ");
+  scanf("%f", &first_n);
+  printf("Agora digite a segunda nota: ");
+  scanf("%f", &second_n);
+  result = (first_n + second_n) / 2;
+
+  if(result >= m) printf("Aluno aprovado! Média: %f.", result);
+  else if(result > 3) printf("Aluno reprovado, mas com chance de recuperação! Média: %f. :c", result);
+  else printf("Aluno reprovado! Média: %f.", result);
+}
+
+void quest_tree() {
+  int user_age;
+  printf("Digite a sua idade: ");
+  scanf("%d", &user_age);
+  if(user_age < 60) printf("Você ainda não chegou na terceira idade. :3");
+  else printf("Você alcançou a terceira idade. :D");
+}
+
+int main(void) {
+  int aux;
+  printf("Digite 1 para a questão 1, 2 para a dois e 3 para a 3 ou qualquer outro número para fechar.\n");
+  scanf("%d", &aux);
+  if(aux == 1) quest_one();
+  else if(aux == 2) quest_two();
+  else if(aux == 3) quest_tree();
+  printf("\n\nEncerrando app...\n");
   return 0;
 }
